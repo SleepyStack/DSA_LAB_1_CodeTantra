8d32d609b5afb41e6e9ceb0047ac0142c2a7b5af
Chirag <chirag2006chopra@gmail.com>
2025-08-26 16:25:37 +0530

    Count element occurrence counting in an array

---
+++ 1.1.5.c
@@ -0,0 +1,43 @@
+/*
+Implement a program to count the occurrence of a specific element 
+k
+ in an array of size 
+n
+
+
+
+Input Format:
+
+The first line of the input represents an integer 
+n
+The second line of the input represents 
+n
+ space separated elements of the array (Integers)
+The third line of the input represents an integer 
+k
+ to search the occurrence in the array.
+Output Format:
+
+The output contains an integer representing the count of the occurrence of integer 
+k
+ in the array.
+*/
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i = 0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int k;
+	int count=0;
+	scanf("%d",&k);
+	for(int i = 0;i<n;i++){
+		if(arr[i]==k){
+			count++;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
